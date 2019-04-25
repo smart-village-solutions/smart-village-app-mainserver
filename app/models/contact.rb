641d36cfb8b088fa129a7b133577e675ee202428
Sebastian Peter <sebastian.peter@ikusei.de>
2019-04-25 15:24:32 +0200

    Add Model for Contact
    * resource contact provides contact information
    * every other main resource in the app (point_of_interest, operating_company, data_provider, event, tour) has contact information and is connected to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ApplicationRecord
+    belongs_to :operating_company, optional: true
+    belongs_to :data_provider, optional: true
+    belongs_to :point_of_interest, optional: true
 end
