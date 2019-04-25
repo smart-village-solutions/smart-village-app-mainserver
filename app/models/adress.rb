49d5b24545d3483fbba06f851ca0f926d66c6c6b
Sebastian Peter <sebastian.peter@ikusei.de>
2019-04-25 15:15:35 +0200

    Create model for adress
    * resource adress is  used by all other resources who neead/have an adress

--- app/models/adress.rb
+++ app/models/adress.rb
@@ -1,3 +1,6 @@
 class Adress < ApplicationRecord
   has_one :geo_location
+  belongs_to :point_of_interests, optional: true
+  belongs_to :operating_company, optional: true
+  belongs_to :data_provider, optional: true
 end
