a055c570db8886b4f22a0371f750002b31fc309c
Sebastian Peter <sebastian.peter@ikusei.de>
2019-04-26 14:53:01 +0200

    Add new database schema and  annotate certificates

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: certificates
+#
+#  id                   :bigint(8)        not null, primary key
+#  name                 :string(255)
+#  point_of_interest_id :bigint(8)
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+
+
 class Certificate < ApplicationRecord
   belongs_to :point_of_interest
 end
