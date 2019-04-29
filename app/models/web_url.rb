f8d61c2fc49b3d4da3a6ee107215731581f0ac0a
Sebastian Peter <sebastian.peter@ikusei.de>
2019-04-29 19:05:00 +0200

    Add validation for web_url
    * add regex for property url to be sure that only urls with standard web url format are saved
    
    *add spec to to test the validation
    
    #5

--- app/models/web_url.rb
+++ app/models/web_url.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class WebUrl < ApplicationRecord
-    belongs_to :web_urlable, polymorphic: true
+  belongs_to :web_urlable, polymorphic: true
+  validates :url, format: { with: %r{\A(http|https)://[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\z}ix }
 end
 
 # == Schema Information
