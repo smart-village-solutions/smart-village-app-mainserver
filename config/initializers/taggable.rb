e04a72149569ae5e2762fac5596d002b6fde562f
Sebastian Peter <sebastian.peter@ikusei.de>
2019-04-30 17:28:53 +0200

    Add act_ass_taggable to POI
    * act as taggable gives the model poi a taglist where tags which come
    from the TMB.xml can be saved
    
     #3

---
+++ config/initializers/taggable.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# initializer necessary because of this:
+# https://github.com/mbleigh/acts-as-taggable-on/issues/623
+ActsAsTaggableOn.force_binary_collation = true
