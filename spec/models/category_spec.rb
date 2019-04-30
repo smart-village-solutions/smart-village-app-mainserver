d351b506d29f852de5e8afac15521c6b49d80674
Sebastian Peter <sebastian.peter@ikusei.de>
2019-04-30 12:57:02 +0200

    Add validation to category model
    * add presence validatzion for attribute name
    * spec presence validation
    
     #5

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
 end
 
 # == Schema Information
