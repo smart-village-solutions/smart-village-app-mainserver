34f5ca7b77bdfffe50fcec73335d1f99b33c5655
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-02 18:16:16 +0200

    Add basic model for news_items and connected content_blocks and spec it
    * add basic model for news according to swagger specification
    *  add basic model for content_blocks. Content blocks are the building blocks for news items
    * spec the relations of both models and add factories for future specs
    
    #8

---
+++ db/migrate/20190502105044_create_news_items.rb
@@ -0,0 +1,15 @@
+class CreateNewsItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :news_items do |t|
+      t.string :author
+      t.string :type
+      t.boolean :full_version
+      t.integer :characters_to_be_shown
+      t.datetime :publication_date
+      t.datetime :published_at
+      t.boolean :show_publish_date
+      t.string :news_type
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/news_items.rb
@@ -0,0 +1,27 @@
+FactoryBot.define do
+  factory :news_item do
+    author { "MyString" }
+    news_type { "" }
+    full_version { true }
+    characters_to_be_shown { 0 }
+    publication_date { "2019-05-02 12:50:44" }
+    published_at { "2019-05-02 12:50:44" }
+    show_publish_date { false }
+  end
+end
+
+# == Schema Information
+#
+# Table name: news_items
+#
+#  id                     :bigint           not null, primary key
+#  author                 :string(255)
+#  type                   :string(255)
+#  full_version           :boolean
+#  characters_to_be_shown :integer
+#  publication_date       :datetime
+#  published_at           :datetime
+#  show_publish_date      :boolean
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
