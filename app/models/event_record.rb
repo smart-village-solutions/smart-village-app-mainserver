c0e047adca649682529de1b06e6b5318f733d237
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-03 18:36:47 +0200

    Fix indentation for EventRecord
    #9

--- app/models/event_record.rb
+++ app/models/event_record.rb
@@ -1,14 +1,14 @@
 class EventRecord < ApplicationRecord
-    belongs_to :category
-    has_many :urls, as: :web_urlable, class_name: "WebUrl"
-    has_one :data_provider, as: :providable
-    has_one :organizer, as: :companyable, class_name: "OperatingCompany"
-    has_one :adress, as: :adressable
-    has_one :location, as: :locateable
-    has_one :contact, as: :contactable
-    has_one :accessibilty_information, as: :accessable
-    has_many :price_informations, as: :priceable, class_name: "Price"
-    has_many :media_contents, as: :mediaable
-    has_one :repeat_duration,
-    has_one :highlight, as: :highlightable
+  belongs_to :category
+  has_many :urls, as: :web_urlable, class_name: "WebUrl"
+  has_one :data_provider, as: :providable
+  has_one :organizer, as: :companyable, class_name: "OperatingCompany"
+  has_one :adress, as: :adressable
+  has_one :location, as: :locateable
+  has_one :contact, as: :contactable
+  has_one :accessibilty_information, as: :accessable
+  has_many :price_informations, as: :priceable, class_name: "Price"
+  has_many :media_contents, as: :mediaable
+  has_one :repeat_duration,
+  has_one :highlight, as: :highlightable
 end
