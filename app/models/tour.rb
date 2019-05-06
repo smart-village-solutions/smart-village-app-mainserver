1a65fca51f42de628d14a3c89e41465838da0575
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-06 17:38:17 +0200

    Add integers which map to enum attributes

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 class Tour < Attraction
   has_many :geometry_tour_data, as: :geo_locateable, class_name: "GeoLocation"
   # eine Tour geht durch mehrere regionen. Eine RegionenTabelle haben wir aber nicht. Was machen.
-  # Doch eine erstellen auf die sich dann auch location bezieht ? Ich würde sagen ja.
-  enum means_of_transportation: %i[bike canoe foot]
+  # Doch eine erstellen auf die sich dann auch location bezieht? Ich würde sagen ja.
+  enum means_of_transportation: [bike: 0, canoe: 1, foot: 2]
 end
