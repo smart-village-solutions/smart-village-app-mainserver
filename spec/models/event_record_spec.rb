ac44019e63cff6096f3b1c41a94a1acc489a213b
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-06 12:04:11 +0200

    Improve EventRecord and create spec
    * fix providable -> provideable
    * EventRecord only has one record but many  adresses
    * recreate Database due to new models
    * TODO: an event has many dates, need to create model for that
    
    #9

--- spec/models/event_record_spec.rb
+++ spec/models/event_record_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe EventRecord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:adresses) }
+  it { is_expected.to have_many(:contacts) }
+  it { is_expected.to have_one(:organizer) }
+  it { is_expected.to have_one(:data_provider) }
+  it { is_expected.to have_many(:price_informations) }
+  it { is_expected.to have_many(:media_contents) }
+  it { is_expected.to have_one(:location) }
+  it { is_expected.to have_one(:highlight) }
+  it { is_expected.to have_one(:repeat_duration) }
+  it { is_expected.to have_one(:accessibilty_information) }
+  it { is_expected.to have_many(:urls) }
+  it { is_expected.to validate_presence_of(:title) }
 end
