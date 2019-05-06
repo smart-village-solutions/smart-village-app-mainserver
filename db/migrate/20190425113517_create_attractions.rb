2af843e4832c2093bab87ec9f8cd7834bbbf3b98
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-06 17:30:38 +0200

    Fix error in migration for attractions table
    * rename filrname from attraction to attractions to make migration work
    
    #10

---
+++ db/migrate/20190425113517_create_attractions.rb
@@ -0,0 +1,15 @@
+class CreateAttractions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :attractions do |t|
+      t.integer :external_id
+      t.string :name
+      t.string :description
+      t.string :mobile_description
+      t.boolean :active, default: true
+      t.integer :length_km
+      t.integer :means_of_transportation
+
+      t.timestamps
+    end
+  end
+end
