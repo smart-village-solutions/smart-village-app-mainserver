934b821827e51d0eb638b0b05cc3d8f9e47e7496
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-06 17:33:19 +0200

    Add different menas of transportation to Tour
    * a tour can be a bike , canoue or a hiking tour
    * good solution for this: make means_of_transportation an enum field with different fixed types for atour
    
    #10

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class Tour < Attraction
   has_many :geometry_tour_data, as: :geo_locateable, class_name: "GeoLocation"
   # eine Tour geht durch mehrere regionen. Eine RegionenTabelle haben wir aber nicht. Was machen.
-  # Doch eine erstellen auf die sich dann auch location bezieht. Ich würde sagen ja.
+  # Doch eine erstellen auf die sich dann auch location bezieht ? Ich würde sagen ja.
+  enum means_of_transportation: %i[bike canoe foot]
 end
