77feed1feb5603dd8536ec479e572f73ef0a53ee
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-07 15:19:53 +0200

    Correct enum for tour
    * correct enum because explicit enum needs to be a hash not an array
    
    #10

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -7,5 +7,5 @@
 class Tour < Attraction
   has_many :geometry_tour_data, as: :geo_locateable, class_name: "GeoLocation"
   has_many :regions, as: :regionable
-  enum means_of_transportation: [bike: 0, canoe: 1, foot: 2]
+  enum means_of_transportation: { bike: 0, canoe: 1, foot: 2 }
 end
