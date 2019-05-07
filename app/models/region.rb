c02903039a78d6eeaa7462000498b3790830db3f
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-07 18:35:21 +0200

    Change regions table
    * regions table had a belongs_to polymorphic association.
    * needs to be changed because one region can have many other resources but one region doesn't exclusively belong to only one other resource
    * commit wil implement this necessary change and recreate Database
    
    #18

--- app/models/region.rb
+++ app/models/region.rb
@@ -4,6 +4,6 @@
 # Table for all regions which are relevant for the smart village app.
 #
 class Region < ApplicationRecord
-  belongs_to :regionable, polymorphic: true
+  has_many :tours
   has_many :locations
 end
