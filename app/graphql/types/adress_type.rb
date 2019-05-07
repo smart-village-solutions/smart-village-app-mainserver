339e269d2b63473aee99c0cdfef65c8e991bbddf
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-07 18:45:36 +0200

    Add Point of Interest Type
    * Add  GraphQL Type for Point of Interest to make root query for all points of interest possible
    * add basic resources adress and geo location to graphql and add them as types to point of interest
    
    #18

---
+++ app/graphql/types/adress_type.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Types
+  class AdressType < Types::BaseObject
+    field :id, ID, null: true
+    field :addition, String, null: true
+    field :street, String, null: true
+    field :city, String, null: true
+    field :zip, String, null: true
+    field :kind, String, null: false
+    field :geo_location, GeoLocationType, null: true
+  end
+end
