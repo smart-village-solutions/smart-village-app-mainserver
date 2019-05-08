39011e405e4eb09f9a74ca3e1723bdbc3678be80
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-08 19:00:50 +0200

    Synchronize ga´raphql types with uodated models
    * implement the new associations for operating_companyand data provider
    * correct scalar types for height,  width and content_type in Media Content Type
    
    #18

--- app/graphql/types/media_content_type.rb
+++ app/graphql/types/media_content_type.rb
@@ -3,9 +3,9 @@ module Types
     field :id, ID, null: false
     field :caption_text, String, null: false
     field :copyright, String, null: true
-    field :height, String, null: true
-    field :width, Boolean, null: true
-    field :content_type, [AdressType], null: false
+    field :height, Integer, null: true
+    field :width, Integer, null: true
+    field :content_type, String, null: false
     field :source_url, WebUrlType, null: false
   end
 end
--- app/graphql/types/operating_company_type.rb
+++ app/graphql/types/operating_company_type.rb
@@ -2,7 +2,7 @@ module Types
   class OperatingCompanyType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: true
-    field :adresses, [AdressType], null: true
+    field :adress, AdressType, null: true
     field :contact, ContactType, null: true
   end
 end
