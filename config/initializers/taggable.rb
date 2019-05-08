31a54ee7c92c97ead7db2d80f05f149c302389e0
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-08 18:19:58 +0200

    Delete Taggable initializer
    * taggable initializer is not necessary anymore because there is a migrationn which does the same
    
    #18

--- config/initializers/taggable.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# initializer necessary because of this:
-# https://github.com/mbleigh/acts-as-taggable-on/issues/623
-ActsAsTaggableOn.force_binary_collation = true
