3493deb0e1833091ae37260407facdd1a49d604c
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-10 18:37:07 +0200

    Correct Migrations and rewrite schema
    * Location still had an attribute  region but needs aregion_id  because it belongs to table region
    *  correct spelling in migration for Fixed Dates
    * recreate database schema
    
    #26

--- db/migrate/20190506101744_create_fixed_dates.rb
+++ db/migrate/20190506101744_create_fixed_dates.rb
@@ -6,7 +6,7 @@ class CreateFixedDates < ActiveRecord::Migration[5.2]
       t.string :weekday
       t.time :time_start
       t.time :time_end
-      t.string :time_Description
+      t.string :time_description
       t.boolean :use_only_time_description
       t.references :dateable, polymorphic: true, index: true
 
