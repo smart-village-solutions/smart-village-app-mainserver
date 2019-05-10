c338e6170f742db4dc5fd990f9c8b5717c20249a
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-10 18:28:52 +0200

    Add Query Types for Event Records
    * add a query for  one event record found by id
    * add a query for all event records
    
    #26

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,6 +6,11 @@ module Types
     field :poi, PointOfInterestType, null: false do
       argument :id, ID, required: true
     end
+    field :all_event_records, [EventRecordType], null: false
+    field :event_record, EventRecordType, null: false do
+      argument :id, ID, required: true
+    end
+
 
     def all_points_of_interest
       PointOfInterest.all
@@ -13,6 +18,14 @@ module Types
 
     def poi(id:)
       PointOfInterest.find(id)
+    end
+
+    def all_event_records
+      EventRecord.all
+    end
+
+    def event_record(id:)
+      EventRecord.find(id)
     end
   end
 end
