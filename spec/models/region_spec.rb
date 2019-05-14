0415591c08c93f983cb6e9f8efd693be6128c5a4
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-14 14:44:42 +0200

    Correct Migration and recreate database
    * tour is not a table but a type of table attraction so joined table needs to be named attractions_regions

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -4,6 +4,7 @@ require "rails_helper"
 
 RSpec.describe Region, type: :model do
   it { is_expected.to have_many(:locations) }
+  it { is_expected.to have_and_belong_to_many(:tours) }
 end
 
 # == Schema Information
