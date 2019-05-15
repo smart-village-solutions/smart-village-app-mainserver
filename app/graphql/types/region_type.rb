eeb808586eea6455fa90f58e6c64e464368e8d79
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-15 14:14:34 +0200

    Add Type and Query for Tour
    * tours queries for all tours with optional limit and order arguments
    * tour queries fo a single tour
    * create RegionType because
     TourType needs it
    
    #39

---
+++ app/graphql/types/region_type.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Types
+  class RegionType < Types::BaseObject
+    field :name, String, null: false
+  end
+end
---
+++ app/graphql/types/tour_type.rb
@@ -0,0 +1,24 @@
+module Types
+  class TourType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :description, String, null: true
+    field :mobile_description, String, null: true
+    field :active, Boolean, null: true
+    field :addresses, [AddressType], null: true
+    field :means_of_transportation, String, null: true
+    field :length_km, Integer, null: true
+    field :category_id, Integer, null: false
+    field :data_provider, DataProviderType, null: true
+    field :tags, String, null: true
+    field :contact, ContactType, null: true
+    field :web_urls, [WebUrlType], null: true
+    field :media_contents, [MediaContentType], null: true
+    field :operating_company, OperatingCompanyType, null: true
+    field :certificates, [CertificateType], null: true
+    field :regions, [RegionType], null: true
+    field :geometry_tour_data, [GeoLocationType], null: false
+    field :updated_at, String, null: true
+    field :created_at, String, null: true
+  end
+end
