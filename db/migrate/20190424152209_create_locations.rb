afc0b2f60687b7d89d0d103aa0a64f19a64a2032
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-15 14:39:11 +0200

    Lint migration for location
    * remove unnecessary whitespace
    
    #39

--- db/migrate/20190424152209_create_locations.rb
+++ db/migrate/20190424152209_create_locations.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateLocations < ActiveRecord::Migration[5.2]
   def change
     create_table :locations do |t|
@@ -7,7 +9,7 @@ class CreateLocations < ActiveRecord::Migration[5.2]
       t.string :state
       t.string :country
       t.references :locateable, polymorphic: true, index: true
-      t.references :region, index:true
+      t.references :region, index: true
       t.timestamps
     end
   end
