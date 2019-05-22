255b27a97f0d1ff6bf728d256077e2fc20814912
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-22 12:55:08 +0200

    Add mutation for resource tour
    * api needs to provide app with information for tours
    * to import these tours from various resources the api graphql server needs a mutation for these resources
    * this commit provides this mutation
    
    #44

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :createTour, mutation: Mutations::CreateTour
     field :create_point_of_interest, mutation: Mutations::CreatePointOfInterest
   end
 end
