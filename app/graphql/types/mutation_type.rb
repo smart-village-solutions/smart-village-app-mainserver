14f8c73713e34ce20673b2d85ee977d4c7739bcd
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-23 13:07:59 +0200

    Add mutation to create a news_item
    * Add mutation class and resolver
    * add necessary input type content blocks
    * add mutation example as graphql file
    * spec mutation
    * update graphql schema
    
    #44

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,7 +2,8 @@
 
 module Types
   class MutationType < Types::BaseObject
-    field :createTour, mutation: Mutations::CreateTour
+    field :create_news_item, mutation: Mutations::CreateNewsItem
+    field :create_tour, mutation: Mutations::CreateTour
     field :create_event_record, mutation: Mutations::CreateEventRecord
     field :create_point_of_interest, mutation: Mutations::CreatePointOfInterest
   end
