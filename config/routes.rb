9af973d46d82b59a55dae402ad9d9d2df665423f
Marco Metz <m.metz@ikusei.de>
2019-05-25 09:15:40 +0200

    adding default root
    Default root to Applications List added

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   end
 
   post "/graphql", to: "graphql#execute"
+
+  root to: "oauth/applications#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
