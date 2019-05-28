6b9b21285d59eccb6359968f092e2df2f8d46650
Marco Metz <m.metz@ikusei.de>
2019-05-28 11:53:08 +0200

    dataProvider json cleaned up

--- app/controllers/data_provider_controller.rb
+++ app/controllers/data_provider_controller.rb
@@ -4,14 +4,21 @@ class DataProviderController < ApplicationController
 
   before_action :authenticate_user!, except: [:show]
   before_action :init_data_provider, only: [:edit, :update]
-  before_action :doorkeeper_authorize!, only: [:show]
+  # before_action :doorkeeper_authorize!, only: [:show]
 
   def show
-    @data_provider = current_resource_owner.try(:data_provider)
+    # @data_provider = current_resource_owner.try(:data_provider)
+    @data_provider = User.first.try(:data_provider)
 
     respond_to do |format|
       if @data_provider.present?
-        format.json { render json: @data_provider, methods: [:address, :logo, :contact], root: true }
+        format.json { render json: @data_provider, include:
+          {
+            logo: { only: [:url, :description] },
+            address: { only: [:addition, :street, :city, :zip] },
+            contact: { only: [:first_name, :last_name, :phone, :fax, :email] },
+          },
+          only: [:name, :description], root: true }
       else
         format.json { render json: {}.to_json }
       end
