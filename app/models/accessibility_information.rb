ad9f1d30af705fe5ec93ceeef091c2ed736e2a87
Marco Metz <m.metz@ikusei.de>
2019-05-28 15:50:14 +0200

    reject empty urls

--- app/models/accessibility_information.rb
+++ app/models/accessibility_information.rb
@@ -4,7 +4,7 @@ class AccessibilityInformation < ApplicationRecord
   belongs_to :accessable, polymorphic: true
   has_many :urls, as: :web_urlable, class_name: "WebUrl"
 
-  accepts_nested_attributes_for :urls
+  accepts_nested_attributes_for :urls, reject_if: ->(attr) { attr[:url].blank? }
 end
 
 # == Schema Information
