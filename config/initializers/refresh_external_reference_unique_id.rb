177df01ea9df522331792160f9814988d2e48af8
René Kersten <rene.kersten@gmail.com>
2019-06-06 18:25:06 +0200

    remove debug output

--- config/initializers/refresh_external_reference_unique_id.rb
+++ config/initializers/refresh_external_reference_unique_id.rb
@@ -3,11 +3,9 @@
 if ActiveRecord::Base.connection.table_exists?("external_references")
   ExternalReference.pluck(:external_type).uniq.each do |type|
     ExternalReference.where(external_type: type).find_each do |entry|
-      puts type
       refreshed_unique_id = entry.external.unique_id
       break if entry.unique_id == refreshed_unique_id
 
-      puts "update #{type}"
       entry.update_column(:unique_id, refreshed_unique_id)
     end
   end
