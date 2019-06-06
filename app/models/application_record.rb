5b037c8b11954526491384e5b39aa796ea80c808
René Kersten <rene.kersten@gmail.com>
2019-06-06 14:28:23 +0200

    fix generate_checksum

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,6 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def generate_checksum(fields)
-    Digest::MD5.hexdigest(fields.sort.join.to_s)
+    Digest::MD5.hexdigest(fields.map(&:to_s).sort.join.to_s)
   end
 end
