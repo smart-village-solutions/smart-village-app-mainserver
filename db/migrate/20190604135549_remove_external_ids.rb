de322096450f6047b19905132f99e0826df240c9
René Kersten <rene.kersten@gmail.com>
2019-06-07 12:21:45 +0200

    disbale RemoveExternalIds Migration

--- db/migrate/20190604135549_remove_external_ids.rb
+++ db/migrate/20190604135549_remove_external_ids.rb
@@ -1,5 +1,5 @@
 class RemoveExternalIds < ActiveRecord::Migration[5.2]
   def change
-    remove_column :event_records, :external_id, :integer
+    # remove_column :event_records, :external_id, :integer
   end
 end
