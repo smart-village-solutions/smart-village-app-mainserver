1a78cfab8705e9c0776df013407ace3048bbd140
René Kersten <rene.kersten@gmail.com>
2019-06-07 11:56:52 +0200

    temporary disbale refresh_external_reference_unique_id initializer

--- config/initializers/refresh_external_reference_unique_id.rb
+++ config/initializers/refresh_external_reference_unique_id.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-Rails.application.config.to_prepare do
-  if ActiveRecord::Base.connection.table_exists?("external_references")
-    ExternalReference.pluck(:external_type).uniq.each do |type|
-      ExternalReference.where(external_type: type).find_each do |entry|
-        refreshed_unique_id = entry.external.unique_id
-        break if entry.unique_id == refreshed_unique_id
+# Rails.application.config.to_prepare do
+#   if ActiveRecord::Base.connection.table_exists?("external_references")
+#     ExternalReference.pluck(:external_type).uniq.each do |type|
+#       ExternalReference.where(external_type: type).find_each do |entry|
+#         refreshed_unique_id = entry.external.unique_id
+#         break if entry.unique_id == refreshed_unique_id
 
-        entry.update_column(:unique_id, refreshed_unique_id)
-      end
-    end
-  end
-end
+#         entry.update_column(:unique_id, refreshed_unique_id)
+#       end
+#     end
+#   end
+# end
