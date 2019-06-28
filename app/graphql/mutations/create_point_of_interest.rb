ef09b4f049ff78930aa5d1649505eaf447f8eeea
Sebastian Peter <sebastian.peter@ikusei.de>
2019-06-28 13:24:12 +0200

    Improve code style

--- app/graphql/mutations/create_point_of_interest.rb
+++ app/graphql/mutations/create_point_of_interest.rb
@@ -15,8 +15,9 @@ module Mutations
                                                 }
     argument :contact, Types::ContactInput, required: false, as: :contact_attributes,
                                             prepare: ->(contact, _ctx) { contact.to_h }
-    argument :price_informations, [Types::PriceInput], required: false, as: :price_informations_attributes,
-                                           prepare: ->(price_informations, _ctx) { price_informations.map(&:to_h) }
+    argument :price_informations, [Types::PriceInput],
+             required: false, as: :price_informations_attributes,
+             prepare: ->(price_informations, _ctx) { price_informations.map(&:to_h) }
     argument :opening_hours, [Types::OpeningHourInput], required: false,
                                                         as: :opening_hours_attributes,
                                                         prepare: lambda { |opening_hours, _ctx|
