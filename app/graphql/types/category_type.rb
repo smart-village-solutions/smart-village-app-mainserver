372b013a2477d0f7b2526dc02428ffa2022d0efd
Daniel Molnar <dm@ikusei.de>
2019-08-02 13:50:45 +0200

    Add fields for model counts to CategoryType
    - we need to group by categories for points of interest or tours in the mobile app
    - we do not want to render empty categories (without any counts)
    - added count fields for both to graphql CategoryType
    - added methods for that fields to model Category
    
    updates #116

--- app/graphql/types/category_type.rb
+++ app/graphql/types/category_type.rb
@@ -4,5 +4,7 @@ module Types
   class CategoryType < Types::BaseObject
     field :id, ID, null: true
     field :name, String, null: true
+    field :points_of_interest_count, Integer, null: true
+    field :tours_count, Integer, null: true
   end
 end
