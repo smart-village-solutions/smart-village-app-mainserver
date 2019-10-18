77de4a21f5825078c0a28738466b80d08cacb48b
Daniel Molnar <dm@ikusei.de>
2019-10-18 12:42:23 +0200

    Use locales in accounts form and do some lintings

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -20,7 +20,9 @@ class AccountsController < ApplicationController
     @user = User.find(params["id"])
     @user.build_data_provider if @user.data_provider.blank?
     DataResourceSetting::DATA_RESOURCES.each do |data_resource|
-      data_resource_settings = @user.data_provider.data_resource_settings.where(data_resource_type: data_resource.to_s)
+      data_resource_settings = @user.data_provider.data_resource_settings.where(
+        data_resource_type: data_resource.to_s
+      )
       data_resource_settings.build if data_resource_settings.blank?
     end
   end
@@ -33,10 +35,13 @@ class AccountsController < ApplicationController
   def create
     @user = User.new(account_params)
 
-
     respond_to do |format|
       if @user.save
-        @user.oauth_applications.create(name: "Zugriff per CMS", redirect_uri: "urn:ietf:wg:oauth:2.0:oob", confidential: true)
+        @user.oauth_applications.create(
+          name: "Zugriff per CMS",
+          redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
+          confidential: true
+        )
         flash[:notice] = "Nutzer wurde erstellt"
         format.html { redirect_to action: :edit, id: @user.id }
       else
@@ -57,14 +62,46 @@ class AccountsController < ApplicationController
 
   def account_params
     values = params.require(:user).permit(
-      :email, :password, :password_confirmation, :role,
+      :email,
+      :password,
+      :password_confirmation,
+      :role,
       data_provider_attributes: [
-        :id, :name, :description,
-        :role_point_of_interest, :role_tour, :role_news_item, :role_event_record,
-        logo_attributes: [:id, :url, :description],
-        address_attributes: [:id, :street, :addition, :zip, :city],
-        contact_attributes: [:id, :first_name, :last_name, :phone, :fax, :email],
-        data_resource_settings_attributes: [:id, :data_resource_type, :display_only_summary, :always_recreate_on_import, :data_provider_id, :only_summary_link_text]
+        :id,
+        :name,
+        :description,
+        :role_point_of_interest,
+        :role_tour,
+        :role_news_item,
+        :role_event_record,
+        logo_attributes: %i[
+          id
+          url
+          description
+        ],
+        address_attributes: %i[
+          id
+          street
+          addition
+          zip
+          city
+        ],
+        contact_attributes: %i[
+          id
+          first_name
+          last_name
+          phone
+          fax
+          email
+        ],
+        data_resource_settings_attributes: %i[
+          id
+          data_resource_type
+          display_only_summary
+          always_recreate_on_import
+          data_provider_id
+          only_summary_link_text
+        ]
       ]
     )
     values.delete_if { |_key, value| value.blank? }
