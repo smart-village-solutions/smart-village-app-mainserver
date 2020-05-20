700bedf8492911180e114f9ab4521e6472b9d72a
Marco Metz <m.metz@ikusei.de>
2020-05-20 14:39:29 +0200

    Bundle exec prefixed
    Gem::LoadError: You have already activated rake 12.3.2, but your Gemfile requires rake 12.3.3

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,10 +7,10 @@ DB=${DB_HOST:-db:3306}
 dockerize -wait tcp://$DB -timeout 30s
 
 npm set audit false
-rake db:migrate
+bundle exec rake db:migrate
 
-rake assets:precompile
-rake graphql:schema:dump
+bundle exec rake assets:precompile
+bundle exec rake graphql:schema:dump
 cp -r /app/public/* /assets/
 rm -f /unicorn.pid
 
