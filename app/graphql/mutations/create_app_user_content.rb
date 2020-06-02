641571f968e1ab8c135704e1201806b747277329
Daniel Molnar <dm@ikusei.de>
2020-06-02 19:00:51 +0200

    WIP: implement spam filter logic for creation of AppUserContent
    - a spam check need to be done in order to not create bad entries and
      send tons of e-mails
    
    SVA-16

--- app/graphql/mutations/create_app_user_content.rb
+++ app/graphql/mutations/create_app_user_content.rb
@@ -9,6 +9,8 @@ module Mutations
     type Types::AppUserContentType
 
     def resolve(**params)
+      return spam_message if spam?
+
       app_user_content = AppUserContent.create(params)
       resource_or_error_message(app_user_content)
     end
@@ -21,6 +23,15 @@ module Mutations
         else
           GraphQL::ExecutionError.new("Invalid input: #{record.errors.full_messages.join(", ")}")
         end
+      end
+
+      def spam_message
+        GraphQL::ExecutionError.new("Spam input")
+      end
+
+      # TODO: implement spam filter logic
+      def spam?
+        false
       end
   end
 end
