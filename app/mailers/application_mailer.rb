89f707d96b54c1b9ca7b862e78cea9b59dd35d0d
Daniel Molnar <dm@ikusei.de>
2020-06-02 18:57:35 +0200

    Setup mailjet
    - updated credentials
    -- every app needs to set the following three credentials:
        `mailjet.default_from` - default sender of mails,
        `mailer.notify_admin.to` - recipient of admin notification mails
    
    SVA-16

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+  default from: Rails.application.credentials.mailjet[:default_from]
+  layout "mailer"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -20,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress JavaScripts and CSS.
   # https://github.com/rmosolgo/graphiql-rails/issues/44#issuecomment-397936805
@@ -56,7 +58,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = %i[request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -70,6 +72,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :mailjet
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -83,7 +87,7 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
-  config.logger = GELF::Logger.new("graylog", 12219, "WAN", { :facility => "Main App Server" })
+  config.logger = GELF::Logger.new("graylog", 12_219, "WAN", facility: "Main App Server")
 
   # if ENV["RAILS_LOG_TO_STDOUT"].present?
   #   logger           = ActiveSupport::Logger.new(STDOUT)
