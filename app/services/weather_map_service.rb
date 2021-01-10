cc6bc0c3216d1086c2f671db88693d9403b858a1
Marco Metz <mm@tpwd.de>
2021-01-10 15:45:49 +0100

    Import Service for OpenWeatherMap
    Service Ito import new data from openweatermap.org. Cleans up old data during import.
    
    Only the latests data has to be stored in database.
    
    SVA-128

---
+++ app/services/weather_map_service.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class WeatherMapService
+  attr_accessor :uri
+
+  def initialize()
+    return if Rails.application.credentials.openweathermap.blank?
+
+    lat = Rails.application.credentials.openweathermap[:lat]
+    lon = Rails.application.credentials.openweathermap[:lon]
+    api_key = Rails.application.credentials.openweathermap[:api_key]
+    base_url = "http://api.openweathermap.org"
+
+    @uri = "#{base_url}/data/2.5/onecall?lat=#{lat}&lon=#{lon}&appid=#{api_key}&units=metric&exclude=minutely&lang=de"
+  end
+
+  # Delete old weather data and import new wether data from
+  # http://api.openweathermap.org
+  #
+  # @return [OpenWeatherMap] latest created OpenWeatherMap object
+  def import
+    request_service = ApiRequestService.new(uri)
+    result = request_service.get_request
+
+    return nil unless result.code == "200"
+    return nil if result.body.blank?
+
+    weather_map = nil
+    OpenWeatherMap.transaction do
+      OpenWeatherMap.destroy_all
+      weather_map = OpenWeatherMap.create(data: JSON.parse(result.body))
+    end
+
+    weather_map
+  end
+end
