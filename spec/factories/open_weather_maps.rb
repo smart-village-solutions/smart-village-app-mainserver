833da43dce9fd03aebbbf4dc706e3ed181d2119d
Marco Metz <mm@tpwd.de>
2021-01-10 15:39:32 +0100

    Adding DataResource OpenWeatherMap
    Small model to store/cache the latest weather data.
    
    SVA-128

---
+++ spec/factories/open_weather_maps.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :open_weather_map do
+    data { "MyText" }
+  end
+end
+
+# == Schema Information
+#
+# Table name: open_weather_maps
+#
+#  id         :bigint           not null, primary key
+#  data       :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
