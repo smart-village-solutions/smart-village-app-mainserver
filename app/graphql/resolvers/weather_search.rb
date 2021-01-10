d9a8ec0cf50b831ee7e057371c92f2b5e1471e51
Marco Metz <mm@tpwd.de>
2021-01-10 15:43:21 +0100

    GraphQL Schema to access weather data by api
    query { weatherMap(){id,lon, lat, current, daily, hourly, alerts, createdAt} }
    
    Runs an importer and returns the latest weather data
    
    SVA-128

---
+++ app/graphql/resolvers/weather_search.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require "search_object/plugin/graphql"
+require "graphql/query_resolver"
+
+class Resolvers::WeatherSearch
+  include SearchObject.module(:graphql)
+
+  scope { OpenWeatherMap.all }
+
+  type types[Types::OpenWeatherMapType]
+
+  class OpenWeatherMapsOrder < ::Types::BaseEnum
+    value "createdAt_ASC"
+    value "createdAt_DESC"
+    value "updatedAt_ASC"
+    value "updatedAt_DESC"
+    value "id_ASC"
+    value "id_DESC"
+  end
+
+  option :limit, type: types.Int, with: :apply_limit
+  option :skip, type: types.Int, with: :apply_skip
+  option :ids, type: types[types.ID], with: :apply_ids
+  option :order, type: OpenWeatherMapsOrder, default: "createdAt_DESC"
+
+  def apply_limit(scope, value)
+    scope.limit(value)
+  end
+
+  def apply_skip(scope, value)
+    scope.offset(value)
+  end
+
+  def apply_ids(scope, value)
+    scope.where(id: value)
+  end
+
+  def apply_order(scope, value)
+    scope.order(value)
+  end
+
+  def apply_order_with_created_at_desc(scope)
+    scope.order("created_at DESC")
+  end
+
+  def apply_order_with_created_at_asc(scope)
+    scope.order("created_at ASC")
+  end
+
+  def apply_order_with_updated_at_desc(scope)
+    scope.order("updated_at ASC")
+  end
+
+  def apply_order_with_updated_at_asc(scope)
+    scope.order("updated_at ASC")
+  end
+
+  def apply_order_with_id_desc(scope)
+    scope.order("id DESC")
+  end
+
+  def apply_order_with_id_asc(scope)
+    scope.order("id ASC")
+  end
+
+  # https://github.com/nettofarah/graphql-query-resolver
+
+  def fetch_results
+    # NOTE: Don't run QueryResolver during tests
+    return super unless context.present?
+
+    GraphQL::QueryResolver.run(OpenWeatherMap, context, Types::OpenWeatherMapType) do
+      super
+    end
+  end
+end
