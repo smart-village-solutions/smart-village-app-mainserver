f73964e9b165212506c67d5285d5f56a60771a07
Marco Metz <mm@tpwd.de>
2021-03-03 12:41:21 +0100

    Securing StartUpScript
    e.g.: GenericItem is not defined in Branch wasteCal

--- config/initializers/refresh_external_reference_unique_id.rb
+++ config/initializers/refresh_external_reference_unique_id.rb
@@ -4,8 +4,12 @@ Rails.application.config.to_prepare do
   if ActiveRecord::Base.connected? && ActiveRecord::Base.connection.table_exists?("external_references")
     ExternalReference.pluck(:external_type).uniq.each do |type|
       ExternalReference.where(external_type: type).find_each do |entry|
-        refreshed_unique_id = entry.external.unique_id
-        break if entry.unique_id == refreshed_unique_id
+        begin
+          refreshed_unique_id = entry.external.unique_id
+          break if entry.unique_id == refreshed_unique_id
+        rescue
+          break
+        end
 
         entry.update_column(:unique_id, refreshed_unique_id)
       end
