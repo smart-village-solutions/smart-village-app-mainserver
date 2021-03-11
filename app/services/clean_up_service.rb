4fa0fdc2a56dc1f8333b55d9aab3944b26a8b011
Marco Metz <mm@tpwd.de>
2021-03-11 14:58:26 +0100

    Lösche alte Daten per Cronjob
    Für jede Datenlieferanten werden für jede Resource alle alten Daten herausgesucht und gelöscht, sofern dies in den ResourceSettings aktiviert ist.
    
    Falls alle Daten gelöscht werden würde, wird vorher abgebrochen.

---
+++ app/services/clean_up_service.rb
@@ -0,0 +1,45 @@
+class CleanUpService
+  def initialize
+    @data_resource_types = DataResourceSetting::DATA_RESOURCES
+    accounts = User.includes(:data_provider).all
+    @data_providers = accounts.map(&:data_provider)
+  end
+
+  def perform
+    @data_providers.each do |data_provider|
+      @data_resource_types.each do |data_resource_type|
+        resource_configs = data_provider.data_resource_settings.where(data_resource_type: data_resource_type.to_s).first
+        next if resource_configs.blank?
+        next if resource_configs.settings.blank?
+
+        cleanup_records = resource_configs.settings.fetch("cleanup_old_records", "false") == "true"
+        next unless cleanup_records
+
+        # die Ressourcen vom aktuellen data_provider und data_resource_type sollen bereinigt werden
+        p "Cleanup records for data_provider #{data_provider.id} and #{data_resource_type}"
+        cleanup_records_for(data_provider, data_resource_type)
+      end
+    end
+  end
+
+  def cleanup_records_for(data_provider, data_resource_type)
+    latest_import_date = ExternalReference
+                           .where(data_provider_id: data_provider.id, external_type: data_resource_type.to_s)
+                           .order(:updated_at)
+                           .last
+                           .updated_at
+
+    # Mitternacht, 2 Tage vor dem letzten Import
+    cleanup_records_to_date = (latest_import_date - 2.days).end_of_day
+    external_references = ExternalReference.where(data_provider_id: data_provider.id, external_type: data_resource_type.to_s)
+    resource_ids = external_references.where("updated_at < ?", cleanup_records_to_date).pluck(:external_id)
+
+    p "Deleting #{resource_ids.count} records of #{data_resource_type}"
+
+    # cancel deletion if all data of this data_provider would be deleted
+    return if external_references.count == resource_ids.count
+
+    data_resource_type.where(id: resource_ids).destroy_all
+  end
+
+end
--- bin/start-cron.sh
+++ bin/start-cron.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 bundle exec rails runner "WasteNotification.new.send_notifications"
+bundle exec rails runner "CleanUpService.new.perform"
