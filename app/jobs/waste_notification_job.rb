cb44c1e4af401b67b201e192618c190c3835d882
Daniel Molnar <dm@ikusei.de>
2021-03-29 12:00:07 +0200

    Fix TypeError: no implicit conversion of Date into String
    - `Date.parse` requires a string to be parsed to a Date but `pickup_date`
      is already a Date, which can be `strftime`d

--- app/jobs/waste_notification_job.rb
+++ app/jobs/waste_notification_job.rb
@@ -13,7 +13,7 @@ class WasteNotificationJob
 
     # Message to send
     title = "Abfallkalender"
-    parsed_date = Date.parse(waste_pickup_time.pickup_date).strftime("%d.%m.%Y")
+    parsed_date = waste_pickup_time.pickup_date.try(:strftime, "%d.%m.%Y")
     body = "#{registration_to_check.street}: Am #{parsed_date} wird #{waste_types[registration_to_check.notify_for_waste_type]["label"]} abgeholt."
     message_options = { title: title, body: body }
     messages = [message_options.merge(to: device.token)]
