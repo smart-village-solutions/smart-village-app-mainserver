c7e32a88357677ecdc9bf6161208d242aa50f404
Daniel Molnar <dm@ikusei.de>
2021-06-16 14:45:05 +0200

    chore(graphql public json file): update content type to json
    - updated field type for `content` of `PublicJsonFileType` from `String`
      to `GraphQL::Types::JSON` to be able to return real jsons
    - updated resolver return value for `content` with parsing string to json

--- app/graphql/types/query_types/public_json_file_type.rb
+++ app/graphql/types/query_types/public_json_file_type.rb
@@ -3,6 +3,6 @@
 module Types
   class QueryTypes::PublicJsonFileType < Types::BaseObject
     field :name, String, null: false
-    field :content, String, null: false
+    field :content, GraphQL::Types::JSON, null: false
   end
 end
