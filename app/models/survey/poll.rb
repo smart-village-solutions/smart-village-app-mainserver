6a91ca2d8879c296ed92b78eada761ac175bef25
Marco Metz <mm@tpwd.de>
2021-06-17 08:59:18 +0200

    Zuweisung zu eine DataProvider
    Eine Umfrage wird von einem DataProvider erstellt.
    Zusätzlich bekommt die umfrage das Attribute :visible.
    Dadurch ergibt sich dann später die Möglichkeit einen Freigabeprozess über Grapqhql und das CMS zu realisieren

--- app/models/survey/poll.rb
+++ app/models/survey/poll.rb
@@ -1,9 +1,25 @@
 class Survey::Poll < ApplicationRecord
+  include FilterByRole
+
   has_one :date, as: :dateable, class_name: "FixedDate", dependent: :destroy
   has_many :questions, class_name: "Survey::Question", foreign_key: "survey_poll_id", dependent: :destroy
+
+  belongs_to :data_provider
 
   store :title, coder: JSON
   store :description, coder: JSON
 
   accepts_nested_attributes_for :date, :questions
 end
+
+# == Schema Information
+#
+# Table name: survey_polls
+#
+#  id          :bigint           not null, primary key
+#  title       :text(4294967295)
+#  description :text(4294967295)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  visible     :boolean          default(TRUE)
+#
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_123650) do
+ActiveRecord::Schema.define(version: 2021_06_17_064433) do
 
   create_table "accessibility_informations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.text "description"
@@ -432,6 +432,8 @@ ActiveRecord::Schema.define(version: 2021_06_16_123650) do
     t.text "description", limit: 4294967295
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "visible", default: true
+    t.integer "data_provider_id"
   end
 
   create_table "survey_questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
