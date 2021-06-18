a824421d48d9ec2b544c2ac897ad0297bed9d8a4
Daniel Molnar <dm@ikusei.de>
2021-06-18 09:59:02 +0200

    feat(graphql for surveys): refactor mutation to create surveys
    - updated types for i18n texts
    - updated access of params while deletion, to manage deletion and
      getting the data in one step
    - updated graphql schema files
    
    SVA2-3

--- app/graphql/mutations/create_survey_poll.rb
+++ app/graphql/mutations/create_survey_poll.rb
@@ -2,14 +2,14 @@
 
 module Mutations
   class CreateSurveyPoll < BaseMutation
-    argument :title, GraphQL::Types::JSON, required: false
-    argument :description, GraphQL::Types::JSON, required: false
+    argument :title, Types::QueryTypes::I18nJSON, required: false
+    argument :description, Types::QueryTypes::I18nJSON, required: false
     argument :date,
              Types::InputTypes::DateInput,
              required: false,
              as: :date_attributes,
              prepare: ->(date, _ctx) { date.to_h }
-    argument :questionTitle, GraphQL::Types::JSON, required: true
+    argument :questionTitle, Types::QueryTypes::I18nJSON, required: true
     argument :response_options, [GraphQL::Types::JSON], required: true
 
     field :survey_poll, Types::QueryTypes::SurveyPollType, null: true
@@ -21,16 +21,17 @@ module Mutations
         params = params.merge(data_provider: context[:current_user].try(:data_provider))
       end
 
+      # delete params that shouldn't be passed to creation, because they are unknown to the model
+      question_title = params.delete(:question_title)
+      response_options = params.delete(:response_options)
+
       # build questions_attributes from question_title and response_options
       params = params.merge(
         questions_attributes: [{
-          title: params[:question_title],
-          response_options_attributes: params[:response_options]
+          title: question_title,
+          response_options_attributes: response_options
         }]
       )
-      # delete params that shouldn't be passed to creation, because they are unknown to the model
-      params.delete(:question_title)
-      params.delete(:response_options)
 
       Survey::Poll.create(params)
     end
