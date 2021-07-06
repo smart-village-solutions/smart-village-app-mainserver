33262702ade72cf2c87c8d2a80487512ebfa2d9a
Daniel Molnar <dm@tpwd.de>
2021-07-06 17:30:31 +0200

    feat(survey comments): add createdAt to GraphQL schema
    - removed unused `amount`
    - added `created_at` to be able to access `createdAt` per GraphQL
    - updated schema files
    
    SVA2-22

--- app/graphql/types/query_types/survey_comment.rb
+++ app/graphql/types/query_types/survey_comment.rb
@@ -5,7 +5,7 @@ module Types
     field :id, ID, null: true
     field :survey_poll_id, ID, null: true
     field :message, String, null: true
-    field :amount, Float, null: true
     field :visible, Boolean, null: true
+    field :created_at, String, null: true
   end
 end
