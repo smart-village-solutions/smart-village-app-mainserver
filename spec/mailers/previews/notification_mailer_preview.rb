7ab3bfa41493b84f9ced3304b135b1b89733277e
Daniel Molnar <dm@tpwd.de>
2021-09-07 17:55:45 +0200

    fix: NotificationMailerPreview for notify_admin
    - fixed failing preview because of missing data with
      adding dummy data to the preview method

--- spec/mailers/previews/notification_mailer_preview.rb
+++ spec/mailers/previews/notification_mailer_preview.rb
@@ -2,7 +2,17 @@
 
 class NotificationMailerPreview < ActionMailer::Preview
   def notify_admin
-    app_user_content = AppUserContent.first_or_create
+    app_user_content = AppUserContent.first_or_create(
+      data_type: "json",
+      data_source: "form",
+      content: {
+        name: "Lorem Ipsum",
+        email: "lorem@ipsum.dolor",
+        phone: "00001212121",
+        message: "Test \n Dolor dolore laborum cillum quis esse enim id.",
+        consent: true
+      }.to_json
+    )
 
     NotificationMailer.notify_admin(app_user_content)
   end
