e3d360c792e268f88c2491f2d4c23bb081d6c08c
Finn Heemeyer <fh@tpwd.de>
2021-12-17 16:56:11 +0100

    Finish StaticContentsController spec
    Refactor testing of devise auth into controller_macros.rb

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -14,4 +14,37 @@ module ControllerMacros
 	def login_admin
 		login_user(true)
 	end
+
+  def test_signed_out(http_method, action, params={})
+    context "when user is logged out" do
+      before do
+        if params[:create_model]
+          entity = FactoryBot.create(params[:create_model])
+          self.send(http_method, action, params: { id: entity.id })
+        else
+          self.send(http_method, action)
+        end
+      end
+
+      it { is_expected.to redirect_to new_user_session_path }
+    end
+  end
+
+  def test_user_signed_in(http_method, action, params={})
+    context "when user is signed in" do
+      login_user
+
+      before do
+        if params[:create_model]
+          entity = FactoryBot.create(params[:create_model])
+          self.send(http_method, action, params: { id: entity.id })
+        else
+          self.send(http_method, action)
+        end
+      end
+
+      it { is_expected.to respond_with :not_found }
+      it { expect(response.body).to include("not allowed") }
+    end
+  end
 end
