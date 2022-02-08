eeefb2ed155d751c36ab94e1f88affdb7550f6c3
Daniel Molnar <dm@tpwd.de>
2022-02-08 12:49:39 +0100

    feat(category): respect ancestry in graphql queries
    - sorted fields for better overview
    - added missing field for `generic_items` as for the other
      fields with `count`s
    - added fields for ancestry methods `parent` and `children`
      to be able to traverse through categories per graphql
    
    SVA-28

--- app/graphql/types/query_types/category_type.rb
+++ app/graphql/types/query_types/category_type.rb
@@ -4,16 +4,20 @@ module Types
   class QueryTypes::CategoryType < Types::BaseObject
     field :id, ID, null: true
     field :name, String, null: true
-    field :generic_items_count, Integer, null: true
-    field :points_of_interest_count, Integer, null: true
-    field :tours_count, Integer, null: true
-    field :news_items_count, Integer, null: true
+    field :parent, QueryTypes::CategoryType, null: true
+    field :children, [QueryTypes::CategoryType], null: true
+
     field :event_records_count, Integer, null: true
-    field :upcoming_event_records_count, Integer, null: true
     field :event_records, [QueryTypes::EventRecordType], null: true
-    field :upcoming_event_records, [QueryTypes::EventRecordType], null: true
-    field :points_of_interest, [QueryTypes::PointOfInterestType], null: true
-    field :tours, [QueryTypes::TourType], null: true
+    field :generic_items_count, Integer, null: true
+    field :generic_items, [QueryTypes::GenericItemType], null: true
+    field :news_items_count, Integer, null: true
     field :news_items, [QueryTypes::NewsItemType], null: true
+    field :points_of_interest_count, Integer, null: true
+    field :points_of_interest, [QueryTypes::PointOfInterestType], null: true
+    field :tours_count, Integer, null: true
+    field :tours, [QueryTypes::TourType], null: true
+    field :upcoming_event_records_count, Integer, null: true
+    field :upcoming_event_records, [QueryTypes::EventRecordType], null: true
   end
 end
