0fb10fb70f6b152503661e57d8d3b08f1af2b879
Marco Metz <mm@tpwd.de>
2022-05-30 11:51:47 +0200

    fix: Absicherung der Cronjobs
    Im Log tauchten unspezifische Meldungen der Form "undefined method `map' for nil:NilClass" auf.
    Eventuell sind sie die Ursache, warum der CronjobService bei Rüdersdorf immer wieder neu startet.

--- app/services/category_service.rb
+++ app/services/category_service.rb
@@ -2,6 +2,7 @@ class CategoryService
   def update_all_defaults
     data_resource_types = DataResourceSetting::DATA_RESOURCES.map(&:to_s)
     data_provider_ids = User.all.map(&:data_provider_id)
+    return if data_provider_ids.blank?
 
     data_provider_ids.each do |data_provider_id|
       data_resource_types.each do |data_resource_type|
@@ -14,12 +15,15 @@ class CategoryService
     data_provider = DataProvider.find_by(id: data_provider_id)
     return if data_provider.blank?
 
-    default_category_ids = data_provider.settings(data_resource_type).try(:default_category_ids).map(&:to_i)
+    data_provider_settings = data_provider.settings(data_resource_type)
+    return if data_provider_settings.blank?
+
+    default_category_ids = data_provider_settings.try(:default_category_ids)
     return if default_category_ids.blank?
 
     data_elements = data_provider.send(data_resource_type.underscore.pluralize)
     data_elements.each do |data_element|
-      category_ids_to_add = default_category_ids - data_element.category_ids
+      category_ids_to_add = default_category_ids.map(&:to_i) - data_element.category_ids
       data_element.categories << Category.where(id: category_ids_to_add) if category_ids_to_add.present? && category_ids_to_add.any?
     end
   end
