155fe990e1dafb8c7108ef05ca0fa336bcf0310a
Marco Metz <mm@tpwd.de>
2022-08-15 15:13:29 +0200

    Initial Deploy of Basic SaaS App
    with access zu sass database server
    
    SVA-682

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,16 +2,13 @@
 
 set -e
 
-DB=${DB_HOST:-db:3306}
+DB=${DB_HOST:-saas-database-service:3306}
 
 dockerize -wait tcp://$DB -timeout 30s
 
 npm set audit false
 bundle exec rake db:migrate
 
-# TODO: Sobald Assets build im Dockerfile ohne DB funktioniert,
-# kann der Schritt hier entfernt werden
-bundle exec rake assets:precompile
 bundle exec rake graphql:schema:dump
 cp -r /app/public/* /assets/
 rm -f /unicorn.pid
