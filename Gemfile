a0dd5a7d715bb5aebb98ae7c3d4dc84424fc3de3
Marco Metz <mm@tpwd.de>
2022-08-15 15:13:29 +0200

    Initial Deploy of Basic SaaS App
    with access zu sass database server
    
    SVA-682

--- Gemfile
+++ Gemfile
@@ -87,6 +87,9 @@ gem "redis"
 gem "bootstrap4-kaminari-views"
 gem "kaminari"
 
+# Rake assets precompile without DB
+gem "activerecord-nulldb-adapter"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem "byebug", platforms: %i[mri mingw x64_mingw]
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,16 +2,11 @@
 
 set -e
 
-DB=${DB_HOST:-db:3306}
-
-dockerize -wait tcp://$DB -timeout 30s
+dockerize -wait tcp://saas-database-mysql:3306 -timeout 30s
 
 npm set audit false
 bundle exec rake db:migrate
 
-# TODO: Sobald Assets build im Dockerfile ohne DB funktioniert,
-# kann der Schritt hier entfernt werden
-bundle exec rake assets:precompile
 bundle exec rake graphql:schema:dump
 cp -r /app/public/* /assets/
 rm -f /unicorn.pid
