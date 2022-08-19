ea37bd7d9fefb3f26560c8232c8356254e1e60c4
Marco Metz <mm@tpwd.de>
2022-08-19 20:57:39 +0200

    Admin Login fix
    sonst sucht er auch bei admins nach einem attribute subdomain....
    
    SVA-682

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,14 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :rememberable, :validatable
+
+  # Diese Methode wird von Devise verwendet um einen User in der Datenbank zu finden
+  # und wird hiermit überschrieben mit einem Filter auf eine Kommune.
+  #
+  # @param [Hash] warden_conditions {:email=>"admin@smart-village.app", :subdomain=>"bad-belzig.server"}
+  #
+  # @return [Scope] User.where...
+  def self.find_for_authentication(warden_conditions)
+    where(:email => warden_conditions[:email]).first
+  end
 end
