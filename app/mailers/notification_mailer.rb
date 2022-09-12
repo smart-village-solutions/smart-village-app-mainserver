42a345aa4e22cb454f7d179657fd80298becdd22
Marco Metz <mm@tpwd.de>
2022-09-12 17:31:26 +0200

    User Municipality Mailjet Settings
    Send Mails from different Mailjet Accounts for echt Municipality
    
    SVA-682

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,26 +4,41 @@ class NotificationMailer < ApplicationMailer
   default template_path: "/mailers/notification"
 
   def notify_admin(notify_admin_content, municipality_id)
+    municipality = Municipality.find_by(id: municipality_id)
+    set_delivery_options(municipality)
+
     @notify_admin_content = notify_admin_content
 
     mail(
-      to: Municipality.find_by(id: municipality_id).settings[:mailer_notify_admin_to],
-      subject: t("mailers.notification.notify_admin.subject")
+      to: municipality.settings[:mailer_notify_admin_to],
+      from: municipality.settings[:mailjet_default_from],
+      subject: t("mailers.notification.notify_admin.subject"),
+      delivery_method_options: delivery_options
     )
   end
 
   def survey_commented(comment, survey, municipality_id)
+    municipality = Municipality.find_by(id: municipality_id)
+    set_delivery_options(municipality)
+
     creator_of_survey = survey.try(:data_provider).try(:user)
 
     return if creator_of_survey.blank?
 
     @survey = survey
     @comment = comment
-    @cms_url = Municipality.find_by(id: municipality_id).settings[:cms_url]
+    @cms_url = municipality.settings[:cms_url]
 
     mail(
       to: creator_of_survey.email,
+      from: municipality.settings[:mailjet_default_from],
       subject: t("mailers.notification.survey_commented.subject")
     )
+  end
+
+  def set_delivery_options(municipality)
+    Mailjet.config.api_key = municipality.settings[:mailjet_api_key]
+    Mailjet.config.secret_key = municipality.settings[:mailjet_api_secret]
+    Mailjet.config.default_from = municipality.settings[:mailjet_default_from]
   end
 end
