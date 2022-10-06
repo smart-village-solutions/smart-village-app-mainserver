63b8136bdce84180f7163a9c6b1d5b4573a5b0de
Daniel Molnar <dm@tpwd.de>
2022-10-06 17:54:24 +0200

    feat: add hidden field to revert default categories
    - checkboxes need a hidden field that will be transmitted
      if nothing is selected to pass that information correctly
      in order to deselect all options
    
    SVA-660

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,19 @@
 module ApplicationHelper
   def render_selectable_categories(categories, form)
-    category_tags = ""
+    base_element = "user[data_provider_attributes][data_resource_settings_attributes][#{form.options[:child_index]}][default_category_ids][]"
+    category_tags = hidden_field_tag(base_element, false)
+
     categories.each do |category, subtree|
-      base_element = "user[data_provider_attributes][data_resource_settings_attributes][#{form.options[:child_index]}][default_category_ids][]"
-      tree_element = check_box_tag base_element, category.id, Array(form.object.default_category_ids).include?(category.id.to_s)
+      tree_element = check_box_tag(
+        base_element,
+        category.id,
+        Array(form.object.default_category_ids).include?(category.id.to_s)
+      )
       tree_element += category.name
       tree_element += render_selectable_categories(subtree, form)
       category_tags << content_tag("li", raw(tree_element))
     end
+
     content_tag("ul", raw(category_tags))
   end
 
