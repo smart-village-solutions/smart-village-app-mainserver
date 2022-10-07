8e2e55fb42a248a8cb41ba0c163c68bc8f98bca7
Daniel Molnar <dm@tpwd.de>
2022-10-07 18:36:30 +0200

    feat: set default roles on account creation
    - added all roles for admin
    - added no role for mobile app
    - added news items role for mowas
    - linted touched files for readability

--- app/services/setup_user_service.rb
+++ app/services/setup_user_service.rb
@@ -1,28 +1,45 @@
 # frozen_string_literal: true
 
 class SetupUserService
-
   REDIRECT_URL = "urn:ietf:wg:oauth:2.0:oob"
 
-  def initialize(provider_name:, email:, application_name:, role:, municipality_id:)
+  def initialize(provider_name:, email:, application_name:, role:, municipality_id:, data_provider_roles:)
     municipality = Municipality.find(municipality_id)
     user_password = SecureRandom.alphanumeric
 
-    data_provider = DataProvider.create(name: provider_name, description: "", logo: create_web_url)
-    user = User.create(email: email, password: user_password, password_confirmation: user_password, role: role, municipality: municipality)
+    data_provider = DataProvider.create(
+      name: provider_name,
+      description: "",
+      logo: create_web_url,
+      roles: data_provider_roles
+    )
+    user = User.create(
+      email: email,
+      password: user_password,
+      password_confirmation: user_password,
+      role: role,
+      municipality: municipality
+    )
     user.data_provider = data_provider
     user.save
 
-    doorkeeper_app = Doorkeeper::Application.new name: application_name, redirect_uri: REDIRECT_URL
+    doorkeeper_app = Doorkeeper::Application.new(name: application_name, redirect_uri: REDIRECT_URL)
     doorkeeper_app.owner = user
     doorkeeper_app.save
 
-    # Store account in 1Password.com
-    OnePasswordService.setup(municipality_id: municipality.id, password: user_password, username: email) if user.id.present? && Rails.env.production?
+    if user.id.present? && Rails.env.production?
+      # Store account in 1Password.com in production
+      OnePasswordService.setup(
+        municipality_id: municipality.id,
+        password: user_password,
+        username: email
+      )
+    end
   end
 
-  def create_web_url
-    WebUrl.create(url: "https://smart-village.solutions/wp-content/uploads/2020/09/Logo-Smart-Village-Solutions-SVS.png")
-  end
+  private
 
+    def create_web_url
+      WebUrl.create(url: "https://smart-village.solutions/wp-content/uploads/2020/09/Logo-Smart-Village-Solutions-SVS.png")
+    end
 end
