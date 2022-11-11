b1b219b649a32db250575779f592588a7dd15657
Daniel Molnar <dm@tpwd.de>
2022-11-11 15:51:54 +0100

    fix(push notifications): move logic from private function in self function
    - error was: `undefined method `notification_devices' for PushNotification:Class`
    
    SVA-710

--- app/services/push_notification.rb
+++ app/services/push_notification.rb
@@ -15,25 +15,18 @@ class PushNotification
   end
 
   def self.send_notifications(message_options = {})
-    notification_devices(message_options).each do |device|
+    notification_devices = Notification::Device.all
+    data_provider_id = message_options[:data_provider_id]
+
+    if data_provider_id.present?
+      notification_devices = Notification::Device.where(
+        "exclude_data_provider_ids IS NULL OR exclude_data_provider_ids NOT LIKE '%- ?\n%'",
+        data_provider_id
+      )
+    end
+
+    notification_devices.each do |device|
       send_notification(device, message_options)
     end
   end
-
-  private
-
-    # if there is a data provider id given, check if there are devices with individual settings
-    # for excluding data providers and filter them out eventually
-    def notification_devices(message_options = {})
-      data_provider_id = message_options[:data_provider_id]
-
-      if data_provider_id.present?
-        return Notification::Device.where(
-          "exclude_data_provider_ids IS NULL OR exclude_data_provider_ids NOT LIKE '%- ?\n%'",
-          data_provider_id
-        )
-      end
-
-      Notification::Device.all
-    end
 end
