77d79f9fd125bf435706461d25508fe206b7516b
Daniel Molnar <dm@tpwd.de>
2022-11-25 15:18:56 +0100

    feat: cleanup notification devices
    - added cleanup if push notification was tried to sent to unregistered device
    - fixed check for `notification_device` in params for `set_notification_device`
      because navigating in the backend to a device failed
    - refactored waste notifications sending of pushs to reduce repetitions
    - moved `if Rails.env.production?` entirely in `send_notification` instead
      of having it at several places, because we do not want to process push notifications
      in development mode (or others)
    
    SVA-816

--- app/jobs/send_single_push_notification_job.rb
+++ app/jobs/send_single_push_notification_job.rb
@@ -19,9 +19,30 @@ class SendSinglePushNotificationJob < ApplicationJob
 
     begin
       feedback = client.send_messages(messages)
-      RedisAdapter.add_push_log(device_token, message_options.merge(date: DateTime.now, payload: feedback.try(:response).try(:body)))
-    rescue => e
-      RedisAdapter.add_push_log(device_token, message_options.merge(rescue_error: "push notification", error: e, date: DateTime.now))
+      payload = feedback.try(:response).try(:body)
+
+      RedisAdapter.add_push_log(
+        device_token,
+        message_options.merge(date: DateTime.now, payload: payload)
+      )
+
+      cleanup_if_unregistered_device(device_token, payload)
+    rescue StandardError => e
+      RedisAdapter.add_push_log(
+        device_token,
+        message_options.merge(rescue_error: "push notification", error: e, date: DateTime.now)
+      )
     end
+  end
+
+  def cleanup_if_unregistered_device(device_token, payload)
+    return if payload.blank?
+    return if payload["data"].blank?
+    return unless payload["data"]["status"] == "error"
+    return unless payload["data"]["message"] == "The recipient device is not registered with FCM."
+    return if payload["data"]["details"].blank?
+    return unless payload["data"]["details"]["error"] == "DeviceNotRegistered"
+
+    Notification::Device.where(token: device_token).destroy_all
   end
 end
