d6fc8fb1a4809f2f3564f855d6da27e3eba00f9d
Daniel Molnar <dm@tpwd.de>
2022-11-28 15:44:34 +0100

    feat: add job for scheduling push notifications
    - based on `WasteNotification` job for sending push notifications
    
    SVA-710

--- bin/start-cron.sh
+++ bin/start-cron.sh
@@ -3,3 +3,4 @@
 bundle exec rails runner "WasteNotification.new.send_notifications"
 bundle exec rails runner "CleanUpService.new.perform"
 bundle exec rails runner "CategoryService.new.update_all_defaults"
+bundle exec rails runner "PushNotification.new.schedule_notifications"
