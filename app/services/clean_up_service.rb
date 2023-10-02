79ccde0a584bfc0550e9b5e73045cf18472c0864
Marco Metz <mm@tpwd.de>
2023-10-02 18:17:56 +0200

    feat: Entfernung doppelter EventRecords
    Das Verfahren könnte auf andere DataResources ausgeweitet werden, nicht nur auf EventRecords.
    Wenn es dem Inhalt nach doppelte Einträge gibt so können alle bis auf den neuesten hiermit entfernt werden.
    
    Verglichen werden dabei die compareable_attributes:
    
    {"parent_id"=>nil,
     "description"=> "Foo",
     "repeat"=>nil,
     "title"=>"Bar",
     "data_provider_id"=>15,
     "external_id"=>"99767869",
     "visible"=>true,
     "recurring"=>false,
     "recurring_weekdays"=>[],
     "recurring_type"=>nil,
     "recurring_interval"=>nil,
     :dates=>
      [{"date_start"=>Sun, 08 Sep 2024,
        "date_end"=>Sun, 08 Sep 2024,
        "weekday"=>nil,
        "time_start"=>Sat, 01 Jan 2000 10:00:00.000000000 CET +01:00,
        "time_end"=>Sat, 01 Jan 2000 18:00:00.000000000 CET +01:00,
        "time_description"=>"",
        "use_only_time_description"=>false,
        "dateable_type"=>"EventRecord"}],
     :addresses=>
      [{"addition"=>"FooBar",
        "city"=>"Prenzlau",
        "street"=>"asdf",
        "zip"=>"12345",
        "kind"=>"default",
        "addressable_type"=>"EventRecord"}]}
    
    SVA-1153

--- app/services/clean_up_service.rb
+++ app/services/clean_up_service.rb
@@ -22,6 +22,35 @@ class CleanUpService
         cleanup_records_for(data_provider, data_resource_type)
       end
     end
+
+    # Entfernt dem Inhalt nach doppelte EventRecords
+    remove_old_duplicates
+  end
+
+  # Entfernt dem Inhalt nach doppelte EventRecords, deren Prüfsumme
+  # aus den Felder von :compareable_attributes identisch ist.
+  # Zusätzlich wird geprüft, dass es zu dem Datensatz keine ExternalReference mehr gibt.
+  def remove_old_duplicates
+    # {
+    #   "2712dc3c21c4eaf0e8b1a04d3f0b162d"=>[18338, 18439],
+    #   "4c2f3a1eadfb52de16ae30195b3f866f"=>[18339, 18440]
+    # }
+    unique_id_counts = Hash.new { |hash, key| hash[key] = [] }
+    deleted_ids = []
+
+    EventRecord.all.map { |ev| unique_id_counts[Digest::MD5.hexdigest(ev.compareable_attributes.map(&:to_s).sort.join.to_s)] << ev.id }
+    duplicated_event_records = unique_id_counts.select { |_uid, records| records.length > 1 }
+
+    duplicated_event_records.each do |_uid, records|
+      # Der Letzte in der Liste wird niemals gelöscht
+      records[0..-2].each do |record_id|
+        external_reference = ExternalReference.where(external_type: "EventRecord", external_id: record_id)
+        EventRecord.find(record_id).destroy if external_reference.blank?
+        deleted_ids << record_id
+      end
+    end
+
+    p deleted_ids
   end
 
   def cleanup_records_for(data_provider, data_resource_type)
