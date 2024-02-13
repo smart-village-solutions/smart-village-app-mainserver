80f3a5c63dcdec6ef79b226d18747f99f3179953
Denys Kulyk <morequalified@gmail.com>
2024-02-13 17:45:41 +0100

    feat(data-providers-api): Setup swagger specs for accounts api controller

--- spec/requests/api/v1/accounts_spec.rb
+++ spec/requests/api/v1/accounts_spec.rb
@@ -1,5 +1,42 @@
-require 'swagger_helper'
+require "swagger_helper"
 
-RSpec.describe 'api/v1/accounts', type: :request do
-  # Specs for the accounts API controller
+RSpec.describe "api/v1/accounts", type: :request do
+  path "/api/v1/accounts" do
+    post "Creates an account" do
+      tags "Accounts"
+      consumes "application/json"
+      parameter name: :account, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          email: { type: :string, format: :email },
+          role: { type: :string },
+          data_type: { type: :string },
+          logo_link: {
+            type: :string,
+            default: "https://smart-village.solutions/wp-content/uploads/2020/09/Logo-Smart-Village-Solutions-SVS.png"
+          }
+        },
+        required: %w[name email role data_type]
+      }
+
+      response "201", "account created" do
+        let(:account) do
+          {
+            name: "New Account",
+            email: "test@example.com",
+            role: "user",
+            data_type: "business_account",
+            logo_link: "http://example.com/logo.png"
+          }
+        end
+        run_test!
+      end
+
+      response "422", "invalid request" do
+        let(:account) { {} }
+        run_test!
+      end
+    end
+  end
 end
