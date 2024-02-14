c6c49fd6d25a4d4ca4740f7bbc3c6efff86a2281
Denys Kulyk <morequalified@gmail.com>
2024-02-14 23:13:43 +0100

    feat(data-providers-api): Updates for the API controllers CRUD actions

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,25 +1,86 @@
 # frozen_string_literal: true
 
 class Api::V1::AccountsController < Api::BaseController
+  before_action :set_data_provider, only: %i[show update destroy]
+
+  USER_PARAMS = %i[
+    email
+    role
+  ].freeze
+
+  DATA_PROVIDER_PARAMS = %i[
+    name
+    description
+    notice
+    data_type
+    roles
+    logo_url
+    logo_description
+    addition
+    city
+    street
+    zip
+    contact_first_name
+    contact_last_name
+    contact_phone
+    contact_fax
+    contact_email
+  ].freeze
+
+  PARAMS = USER_PARAMS + DATA_PROVIDER_PARAMS + [{ roles: {} }]
+
   def show
-    account = DataProvider.find(params[:id])
-    render json: account
+    render_account_response(@data_provider)
   end
 
   def create
-    account = Accounts::CreateAccountService.new(account_params: account_params).create_account
-    if account
-      render json: account
-    else
-      render json: { errors: account.errors.full_messages }, status: :unprocessable_entity
-    end
+    account = Accounts::CreateAccountService.new(
+      account_params: account_params,
+      municipality_id: MunicipalityService.municipality_id
+    ).create_account
+
+    render_account_response(account, :created)
+  end
+
+  def update
+    account = Accounts::UpdateAccountService.new(
+      data_provider_id: @data_provider.id,
+      account_params: account_params
+    ).update_account
+
+    render_account_response(account)
+  end
+
+  def destroy
+    account = Accounts::DestroyAccountService.new(data_provider_id: @data_provider.id).destroy_account
+    render_account_response(account, :no_content)
   end
 
   private
 
     def account_params
-      params.require(:account).permit(
-        :name, :email, :role, :data_type, :logo_link
-      )
+      params.require(:account).permit(*PARAMS)
+    end
+
+    def set_data_provider
+      @data_provider = DataProvider.find_by(id: params[:id])
+      render_not_found unless @data_provider
+    end
+
+    def render_account_response(account, status = :ok)
+      if account
+        render json: { account: serialize_account(account) }, status: status
+      else
+        render json: { errors: account.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    def serialize_account(account)
+      account.as_json(include: {
+        user: { only: %i[email role] },
+        logo: { only: %i[url description] },
+        address: { only: %i[addition street city zip] },
+        contact: { only: %i[first_name last_name phone fax email] }
+      }, only: %i[id name description notice data_type roles])
     end
 end
