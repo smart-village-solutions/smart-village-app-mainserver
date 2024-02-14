75365b597b2ec856f2ad7f3fa684b24989fa3919
Denys Kulyk <morequalified@gmail.com>
2024-02-14 23:20:44 +0100

    feat(data-providers-api): Setup service specs

---
+++ spec/services/accounts/destroy_account_service_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Accounts::DestroyAccountService, type: :service do
+  # Specs for DestroyAccountService will be here
+end
---
+++ spec/services/accounts/update_account_service_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Accounts::UpdateAccountService, type: :service do
+  # Specs for UpdateAccountService will be here
+end
