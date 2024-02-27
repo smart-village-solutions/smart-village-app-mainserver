d093b87f008c371c333555e890cbb38e82a0a0f3
Marco Metz <mm@tpwd.de>
2024-02-27 14:26:32 +0100

    feat(profile): Stay logged in after sign up
    https://github.com/baschtl/devise-token_authenticatable
    
    enable reset of the authentication token before the model is saved,
    defaults to false
    config.should_reset_authentication_token = false
    
    This is the only way i found to keep the auth token after changing anything on model Member.
    I also extended the validation time of the token to 1 month. After this time the Member hast to log in again.
    
    MQGB-116

--- config/initializers/devise_token.rb
+++ config/initializers/devise_token.rb
@@ -2,7 +2,7 @@ Devise::TokenAuthenticatable.setup do |config|
   # enables the expiration of a token after a specified amount of time,
   # requires an additional field on the model: `authentication_token_created_at`
   # defaults to nil
-  config.token_expires_in = 1.day
+  config.token_expires_in = 1.month
   config.should_ensure_authentication_token = true
-  config.should_reset_authentication_token = true
+  config.should_reset_authentication_token = false
 end
