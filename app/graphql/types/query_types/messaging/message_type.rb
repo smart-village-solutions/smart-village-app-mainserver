5acf9d003fbda3bc704cc57e3953f1e24f65f0f2
Denys Kulyk <morequalified@gmail.com>
2024-03-05 23:08:43 +0100

    feat(MQGB-117-add-messaging-system): Update message type in query types MQGB-[117]

--- app/graphql/types/query_types/messaging/message_type.rb
+++ app/graphql/types/query_types/messaging/message_type.rb
@@ -3,7 +3,7 @@
 module Types
   class QueryTypes::Messaging::MessageType < Types::BaseObject
     field :conversation_id, ID, null: false
-    field :member_id, ID, null: true, description: "This is sender identifier"
+    field :sender_id, ID, null: true
     field :message_text, String, null: true
   end
 end
