d66796be87114e1417df5011cb664a5d32c0d649
Denys Kulyk <morequalified@gmail.com>
2024-04-25 13:16:50 +0200

    feat(GOS-32-extended-resource-filter-and-push-notification): Add snet_push_notifications_at to generic items as well GOS-[32]

--- db/migrate/20240418153949_add_push_notifications_sent_at_to_resources.rb
+++ db/migrate/20240418153949_add_push_notifications_sent_at_to_resources.rb
@@ -4,10 +4,12 @@ class AddPushNotificationsSentAtToResources < ActiveRecord::Migration[6.1]
   def up
     add_column :event_records, :push_notifications_sent_at, :datetime
     add_column :attractions, :push_notifications_sent_at, :datetime
+    add_column :generic_items, :push_notifications_sent_at, :datetime
   end
 
   def down
     remove_column :event_records, :push_notifications_sent_at
     remove_column :attractions, :push_notifications_sent_at
+    remove_column :generic_items, :push_notifications_sent_at
   end
 end
