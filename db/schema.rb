320de05b94e4a4eb3d1e9a9e99366283ffcfd88d
Denys Kulyk <morequalified@gmail.com>
2024-04-25 15:52:56 +0200

    feat(GOS-32-extended-resource-filter-and-push-notification): Remove external services from schema GOS-[32]

--- db/schema.rb
+++ db/schema.rb
@@ -274,41 +274,6 @@ ActiveRecord::Schema.define(version: 2024_04_18_153949) do
     t.index ["external_id", "external_type"], name: "index_external_references_on_id_and_type"
   end
 
-  create_table "external_service_categories", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.string "external_id"
-    t.bigint "external_service_id", null: false
-    t.bigint "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_external_service_categories_on_category_id"
-    t.index ["external_service_id"], name: "index_external_service_categories_on_external_service_id"
-  end
-
-  create_table "external_service_credentials", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.text "client_key"
-    t.text "client_secret"
-    t.string "scopes"
-    t.string "auth_type"
-    t.text "external_id"
-    t.bigint "external_service_id", null: false
-    t.bigint "data_provider_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.text "additional_params"
-    t.index ["data_provider_id"], name: "index_external_service_credentials_on_data_provider_id"
-    t.index ["external_service_id"], name: "index_external_service_credentials_on_external_service_id"
-  end
-
-  create_table "external_services", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "base_uri"
-    t.text "resource_config"
-    t.integer "municipality_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "auth_path"
-  end
-
   create_table "fixed_dates", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.date "date_start"
     t.date "date_end"
@@ -842,10 +807,6 @@ ActiveRecord::Schema.define(version: 2024_04_18_153949) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "external_service_categories", "categories"
-  add_foreign_key "external_service_categories", "external_services"
-  add_foreign_key "external_service_credentials", "data_providers"
-  add_foreign_key "external_service_credentials", "external_services"
   add_foreign_key "messaging_conversation_participants", "members"
   add_foreign_key "messaging_conversation_participants", "messaging_conversations", column: "conversation_id"
   add_foreign_key "messaging_messages", "members", column: "sender_id"
