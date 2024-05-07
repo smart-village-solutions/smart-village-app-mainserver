cbcfbb8af7ac2e4979fb594ca167dfa98dbe5bbf
Marco Metz <mm@tpwd.de>
2024-05-07 16:45:25 +0200

    securing ES6+ support in js compressor
    https://app.rollbar.com/a/SmartVillage/fix/item/SmartVillage-SaaS/86?utm_campaign=exp_repeat_item_message&utm_medium=slack&utm_source=rollbar-notification

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w( jsoneditor.scss )
+
+Rails.application.config.assets.js_compressor = Uglifier.new(harmony: true)
