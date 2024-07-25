527cfb84b4e2e413bc02a8e4e5e34c3dd133e519
Daniel <dm@tpwd.de>
2024-07-25 16:22:23 +0200

    fix: health check

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class HealthCheckController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   # get "health-check"
   def show
