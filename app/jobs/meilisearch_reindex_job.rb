a88b135159c2f0f02e46673aa71050f49912690c
Marco Metz <mm@tpwd.de>
2024-08-02 14:59:16 +0200

    adding meilsearch include sopces
    for better performance on indexing data

--- app/jobs/meilisearch_reindex_job.rb
+++ app/jobs/meilisearch_reindex_job.rb
@@ -8,7 +8,6 @@ class MeilisearchReindexJob < ApplicationJob
       MunicipalityService.municipality_id = municipality.id
       MeiliSearch::Rails.configuration[:timeout] = 3000
       EventRecord.in_batches(of: 10).each_record(&:index!)
-      EventRecord.in_batches(of: 10).each_record(&:index!)
       NewsItem.in_batches(of: 10).each_record(&:index!)
       GenericItem.in_batches(of: 10).each_record(&:index!)
       PointOfInterest.in_batches(of: 10).each_record(&:index!)
