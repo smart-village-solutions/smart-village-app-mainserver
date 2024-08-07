450c3ff627f146a56ac04164d367952a480e81c5
Marco Metz <mm@tpwd.de>
2024-08-07 16:37:00 +0200

    fix: Securing Access of Graphql
    - adding error response if query has empty () Statements, which is not allowd in graphQL
    - securing access on lookaheads
    - code style improvements
    
    SVA-1366

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -11,38 +11,50 @@ class GraphqlController < ApplicationController
     variables = ensure_hash(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
+
     context = {
-      # Query context goes here, for example:
       current_user: current_resource_owner,
       current_member: @current_member,
-      extras: { lookahead: GraphQL::Query.new(SmartVillageAppMainserverSchema, query).lookahead }
-
-      # NOTE: if we want to have more data, we can add the `request` here, for example:
-      # request: request
+      extras: { lookahead: lookaheads(query) }
     }
-
-    result = SmartVillageAppMainserverSchema.execute(
-      query,
-      variables: variables,
-      context: context,
-      operation_name: operation_name
-    )
+    result = if valid_query?(query)
+               SmartVillageAppMainserverSchema.execute(
+                 query,
+                 variables: variables,
+                 context: context,
+                 operation_name: operation_name
+               )
+             else
+               { errors: ["Invalid query: empty () found"] }
+             end
 
     log_graphql_execution_error(result, query)
     render json: result
-  rescue StandardError => error
-    raise error unless Rails.env.development?
+  rescue StandardError => e
+    raise e unless Rails.env.development?
 
-    handle_error_in_development error
+    handle_error_in_development e
   end
 
   private
+
+    def lookaheads(query)
+      return nil unless valid_query?(query)
+
+      GraphQL::Query.new(SmartVillageAppMainserverSchema, query).lookahead
+    end
+
+    def valid_query?(query)
+      # Einfache Überprüfung, ob die Abfrage keine leeren Klammern enthält
+      !query.match(/\(\s*\)/)
+    end
 
     def member_by_token
       member_token = request.env["HTTP_X_AUTHORIZATION"]
       return if member_token.blank?
 
-      @current_member = Member.find_by(municipality_id: MunicipalityService.municipality_id, authentication_token: member_token)
+      @current_member = Member.find_by(municipality_id: MunicipalityService.municipality_id,
+                                       authentication_token: member_token)
     end
 
     def current_resource_owner
@@ -52,10 +64,10 @@ class GraphqlController < ApplicationController
 
     def log_graphql_execution_error(result, query)
       result_hash = result.to_h
-      if result_hash.key?("errors")
-        error_hash = { GraphQLServerError: result_hash, payload: query }
-        logger.error(error_hash)
-      end
+      return unless result_hash.key?("errors")
+
+      error_hash = { GraphQLServerError: result_hash, payload: query }
+      logger.error(error_hash)
     end
 
     # Handle form data, JSON body, or a blank value
