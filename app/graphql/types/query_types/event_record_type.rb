a9035e50a15a1a65dbd1d18465b0cbf59c7f4db1
Daniel <dm@tpwd.de>
2024-10-23 17:29:14 +0200

    feat(events): always use list_date instead of sort_date
    - sort date cannot consider multi dates for an event
      and would always return the first date
    
    SVAK-135

--- app/graphql/types/query_types/event_record_type.rb
+++ app/graphql/types/query_types/event_record_type.rb
@@ -38,9 +38,7 @@ module Types
     field :announcements, [Types::QueryTypes::ShoutType], null: true
 
     def list_date
-      return object.try(:list_date) if object.recurring?
-
-      object.try(:sort_date).try(:strftime, "%Y-%m-%d")
+      object.try(:list_date) || object.try(:sort_date).try(:strftime, "%Y-%m-%d")
     end
 
     def announcements
